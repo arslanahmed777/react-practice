f0fb076809fe583c981dbdb4c652ea13198f3deb
Arslan Ahmed <ashi3610@gmail.com>
2022-05-17 18:36:40 +0500

    remove some code

--- src/components/ValidatoorComponent/MyValidator.js
+++ src/components/ValidatoorComponent/MyValidator.js
@@ -1,6 +1,6 @@
 import ValidationTypes from "./ValidationTypes";
 import isNull from "./isNull";
-const MyValidator = (data, type, Error, validationType) => {
+const MyValidator = (data, type, Error,) => {
     if (type === ValidationTypes.NullCheck) {
         console.log(data);
         if (Object.values(data).some((val) => !isNull(val))) {
@@ -9,29 +9,25 @@ const MyValidator = (data, type, Error, validationType) => {
             return false
         }
     }
-    if (validationType === "custom") {
 
-    }
-    else {
-        if (data instanceof Array) {
 
+
+
+    let error = ""
+    for (let i = 0; i < type.length; i++) {
+        if (type.includes(ValidationTypes.required) && data === "") {
+            error = (<div className='invalid-feedback'>{Error[type.indexOf(ValidationTypes.required)]}</div>);
+            break;
         }
-        else {
-            let error = ""
-            for (let i = 0; i < type.length; i++) {
-                if (type.includes(ValidationTypes.required) && data === "") {
-                    error = (<div className='invalid-feedback'>{Error[type.indexOf(ValidationTypes.required)]}</div>);
-                    break;
-                }
-                if (type.includes(ValidationTypes.capitalize) && data !== data.toUpperCase()) {
-                    error = (<div className='invalid-feedback'>{Error[type.indexOf(ValidationTypes.capitalize)]}</div>);
-                    break;
-                }
-            }
-            return error
+        if (type.includes(ValidationTypes.capitalize) && data !== data.toUpperCase()) {
+            error = (<div className='invalid-feedback'>{Error[type.indexOf(ValidationTypes.capitalize)]}</div>);
+            break;
         }
-
     }
+    return error
+
+
+
 
 }
 
