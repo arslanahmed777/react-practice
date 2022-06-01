eb2a6927ff505247b6f637b033c40e2bcf869ed4
Arslan Ahmed <ashi3610@gmail.com>
2022-06-01 21:56:23 +0500

    working on super popup

--- src/components/SuperPopup/SuperPopup.jsx
+++ src/components/SuperPopup/SuperPopup.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import styles from "./SuperPopup.module.css"
-const SuperPopup = ({ children, zIndex }) => {
+const SuperPopup = ({ children, zIndex, size, position }) => {
     return (
         <div className={styles.Main_Wrapper} style={{ zIndex: zIndex ? zIndex : 3 }}>
             <div className={styles.Mini_Wrapper}>
-                <div className={styles.body}>
+                <div className={`${styles.body} ${styles.animate} ${position === "center_center" ? styles.center_center : ""}`}>
                     {children}
                 </div>
 
             </div>
 
-        </div>
+        </div >
     )
 }
 
